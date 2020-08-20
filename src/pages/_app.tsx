1fdae53cabcc523f77d6f0d574bc10ed5afff95c
Augusto Dias <augustodg2@gmail.com>
2020-08-20 13:17:16 -0300

    styled components setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from 'next/app'
+import GlobalStyles from 'styles/global'
+import Head from 'next/head'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <meta
+          name="description"
+          content="A simple project starter to work with Typescript, React, NextJS and Styled Components"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,5 @@
-import Head from 'next/head'
+import Main from 'components/Main'
 
-type Props = {
-  title: string
-}
-
-export default function Home({ title = 'React Avançado' }: Props) {
-  return (
-    <div>
-      <Head>
-        <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <h1>{title}</h1>
-    </div>
-  )
+export default function Home() {
+  return <Main />
 }
