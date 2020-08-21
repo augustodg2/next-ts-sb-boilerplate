32a1c3ae602d7813bc18e3437df357964d77ba10
Augusto Dias <augustodg2@gmail.com>
2020-08-21 16:56:22 -0300

    fix: remove knobs from storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs'
-  ]
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
 }
